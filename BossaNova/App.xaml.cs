168c03f377152a1afe8d44271b984f7afe069ec4
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2023-08-29 18:03:38 -0400

    added BN folder

---
+++ BossaNova/App.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.Shell;
+using Tasks.Show.Helpers;
+using Tasks.Show.ViewModels;
+
+namespace Tasks.Show 
+{
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    public partial class App : Application, ISingleInstanceApp
+    {
+        public static AssemblyAttributes? Attribs;
+        private Root m_root;
+        private static ILogger _logger = null;
+        public static ILogger Logger
+        {
+            get
+            {
+                if (_logger is null)
+                    _logger = GetLogger(LogLevel.Debug);
+
+                return _logger;
+            }
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance("Tasks.Show"))
+            {
+                AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomainUnhandledException);
+
+                var application = new App();
+
+                application.Init();
+                application.Run();
+
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        public static Root Root { get { return ((App)App.Current).m_root; } }
+    
+        public void Init()
+        {
+            this.InitializeComponent();
+
+            try
+            {
+                m_root = new Root(Storage.Load(), FindResource("FolderColors") as IEnumerable<Color>);
+                
+                Attribs = new AssemblyAttributes();
+
+                #region [Log Dependencies]
+                Logger.WriteLine($"Listing assemblies...", LogLevel.Debug);
+                var assems = Attribs.GetAllAssemblies();
+                foreach (KeyValuePair<string, Version> assem in assems)
+                    Logger.WriteLine($"{assem.Key}: {assem.Value}", LogLevel.Debug);
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "App.Init", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        #region ISingleInstanceApp Members
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // handle command line arguments from external execution
+            return ((MainWindow)MainWindow).ProcessCommandLineArgs(args, false);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Handle application object exceptions. (main UI thread only)
+        /// </summary>
+        private void ApplicationDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Debug.WriteLine($"Unhandled exception from Dispatcher {e.Dispatcher.Thread.ManagedThreadId}: {e.Exception.Message}");
+                System.Diagnostics.Debug.WriteLine(ToLogString(e.Exception, "Full Error Dump"));
+                MessageBox.Show($"{e.Exception}", "UnhandledException", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Logger.WriteLine($"{e.Exception.Message}", LogLevel.Critical);
+                //System.Diagnostics.EventLog.WriteEntry(SystemTitle, $"Unhandled exception thrown from Dispatcher {e.Dispatcher.Thread.ManagedThreadId}: {e.Exception.ToString()}");
+                e.Handled = true;
+            }
+            catch (Exception) { }
+        }
+
+        /// <summary>
+        /// Handle exceptions thrown from custom threads.
+        /// </summary>
+        static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception? ex = e.ExceptionObject as Exception;
+                if (ex != null)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Thread exception: {ex}");
+                    MessageBox.Show($"{ex.Message}", "ThreadException", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    Logger.WriteLine($"Thread exception: {ex}", LogLevel.Critical);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        /// <summary>
+        /// Logging Service Provider (leaving it here for convenience).
+        /// </summary>
+        /// <returns><see cref="ILogger"/></returns>
+        public static ILogger GetLogger(LogLevel level = LogLevel.Debug)
+        {
+            //var logPaths = System.IO.DriveInfo.GetDrives().Where(di => (di.DriveType == DriveType.Fixed) && (di.IsReady) && (di.AvailableFreeSpace > 1000000)).Select(di => di.RootDirectory).OrderByDescending(di => di.FullName);
+            //string root = logPaths.FirstOrDefault().FullName; // "D:\"
+            return new FileLogger(System.IO.Path.Combine(FileLogger.GetRoot(), "Logs"), level);
+        }
+
+        /// <summary>
+        /// Creates a loggable string from an <see cref="Exception"/>.
+        /// The result includes the stacktrace, inner exception, et al.
+        /// </summary>
+        /// <param name="ex">The exception to create the string from.</param>
+        /// <param name="additionalMessage">Additional message to place at the top of the string, may be empty or null.</param>
+        /// <returns>formatted string</returns>
+        public static string ToLogString(Exception ex, string additionalMessage = "")
+        {
+            System.Text.StringBuilder msg = new System.Text.StringBuilder();
+
+            if (!string.IsNullOrEmpty(additionalMessage))
+            {
+                msg.Append($"-----[{additionalMessage}]-----");
+                msg.Append(Environment.NewLine);
+            }
+            else
+            {
+                msg.Append($"-----[{DateTime.Now.ToString("hh:mm:ss.fff tt")}]-----");
+                msg.Append(Environment.NewLine);
+            }
+
+            if (ex != null)
+            {
+                try
+                {
+                    Exception orgEx = ex;
+                    msg.Append("[Exception]: ");
+                    while (orgEx != null)
+                    {
+                        msg.Append(orgEx.Message);
+                        msg.Append(Environment.NewLine);
+                        orgEx = orgEx.InnerException;
+                    }
+
+                    if (ex.Source != null)
+                    {
+                        msg.Append("[Source]: ");
+                        msg.Append(ex.Source);
+                        msg.Append(Environment.NewLine);
+                    }
+
+                    if (ex.Data != null)
+                    {
+                        foreach (object i in ex.Data)
+                        {
+                            msg.Append("[Data]: ");
+                            msg.Append(i.ToString());
+                            msg.Append(Environment.NewLine);
+                        }
+                    }
+
+                    if (ex.StackTrace != null)
+                    {
+                        msg.Append("[StackTrace]: ");
+                        msg.Append(ex.StackTrace.ToString());
+                        msg.Append(Environment.NewLine);
+                    }
+
+                    if (ex.TargetSite != null)
+                    {
+                        msg.Append("[TargetSite]: ");
+                        msg.Append(ex.TargetSite.ToString());
+                        msg.Append(Environment.NewLine);
+                    }
+
+                    Exception baseException = ex.GetBaseException();
+                    if (baseException != null)
+                    {
+                        msg.Append("[BaseException]: ");
+                        msg.Append(ex.GetBaseException());
+                    }
+                }
+                catch (Exception iex)
+                {
+                    System.Diagnostics.Debug.WriteLine($"ToLogString: {iex.Message}");
+                }
+            }
+            return msg.ToString();
+        }
+    }
+}
---
+++ BossaNova/DialogBubble.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Input;
+using Tasks.Show.Models;
+
+namespace Tasks.Show
+{
+    /// <summary>
+    /// Interaction logic for DialogBubble.xaml
+    /// </summary>
+    public partial class DialogBubble : Window
+    {
+        private System.Media.SoundPlayer _ding = null;
+        private DispatcherTimer _tmrClose = null;
+        public string _message { get; set; }
+        public string _title { get; set; }
+        public int _timer { get; set; }
+        public Task _task { get; set; }
+
+        public event Action<string> OnFolderRequest = (name) => { };
+
+        public DialogBubble(string message, Task task, string title = "", int timer = 0)
+        {
+            InitializeComponent();
+            _message = message;
+            _title = title;
+            _timer = timer;
+            _task = task;
+            _ding = new System.Media.SoundPlayer(Tasks.Show.Properties.Resources.ding);
+
+            this.Loaded += (s, e) => 
+            { 
+                this.Title = string.IsNullOrEmpty(_title) ? $"Due on " + _task.Due.Value.ToString("dddd") + $" ({_task.FolderName})" : _title;
+                if (_ding != null) { _ding.Play(); }
+                if (App.Root.WindowHost.IsClosing) { Close(); }
+            };
+        }
+
+        private void NonRectangularWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbMessage.Text = _message;
+            //tbTitle.Text = string.IsNullOrEmpty(_title) ? $"Due on " + _task.Due.Value.ToString("dddd") + $" ({_task.FolderName})" : _title; //"dddd M/d/yy"
+            btnTitle.Content = string.IsNullOrEmpty(_title) ? $"Due on " + _task.Due.Value.ToString("dddd") + $" ({_task.FolderName})" : _title; //"dddd M/d/yy"
+
+            if (_timer > 0)
+            {
+                _tmrClose = new System.Windows.Threading.DispatcherTimer();
+                _tmrClose.Interval = TimeSpan.FromSeconds(_timer);
+                _tmrClose.Tick += tmrClose_Tick;
+                _tmrClose.Start();
+            }
+
+        }
+
+        /// <summary>
+        /// If the user drags the dialog then assume they want to read it.
+        /// </summary>
+        private void NonRectangularWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _tmrClose?.Stop();
+            DragMove();
+        }
+
+        /// <summary>
+        /// If the user hovers over the dialog then assume they want to read it.
+        /// This could become annoying to the user, so I've disabled it.
+        /// </summary>
+        private void NonRectangularWindow_MouseEnter(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Event disabled for convenience.");
+            //_tmrClose?.Stop();
+        }
+
+        private void tmrClose_Tick(object sender, EventArgs e)
+        {
+            _tmrClose?.Stop();
+            Close();
+        }
+
+        private void CloseButtonRectangle_Click(object sender, RoutedEventArgs e)
+        {
+            tbMessage.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() => { tbMessage.Text = "Closing..."; }));
+            _tmrClose?.Stop();
+            Close();
+        }
+
+        /// <summary>
+        /// Go to the folder in the main task view.
+        /// </summary>
+        private void NonRectangularWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            OnFolderRequest?.Invoke($"{_task.FolderName}");
+        }
+    }
+}
