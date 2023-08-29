4e8bc7e8a9ec7e0a31a657129cba50961a366aa9
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2023-08-29 18:00:37 -0400

    added data folder

---
+++ Tasks.Show.Data/Models/FolderChangingEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Tasks.Show.Models
+{
+    public class FolderChangingEventArgs : CancelEventArgs
+    {
+        public FolderChangingEventArgs(Folder newFolder) : base()
+        {
+            m_folder = newFolder;
+        }
+
+        public Folder Folder 
+        { 
+            get => m_folder;
+        }
+
+        private readonly Folder m_folder;
+    }
+}
---
+++ Tasks.Show.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tasks.Show")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Chamware")]
+[assembly: AssemblyProduct("Tasks.Show")]
+[assembly: AssemblyCopyright("Copyright © 2022-2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ae313f0-948b-479b-9de5-88a083773a5e")]
+
+// The data model has not chnaged since the 2010
+// inception, however small tweaks were made.
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
